874574faec4ce617caed16f18ee1f90d98efe959
vitochibere <vitorsantos14@icloud.com>
2026-09-10 05:37:41 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/alimentamais/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.alimentamais.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/alimentamais/AlimentaMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alimentamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentaMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
